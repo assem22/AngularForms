accbbb9ed3fb5a8c1769f65605f76d8cd0aceecb
assem22 <assem.mukhamadi@mail.ru>
2020-11-25 12:06:55 +0600

    1

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-template-driven-form',
@@ -8,47 +7,7 @@ import {NgForm} from '@angular/forms';
 })
 export class TemplateDrivenFormComponent implements OnInit {
 
-  form: Form;
-
   ngOnInit(): void {
 
   }
-
-  // tslint:disable-next-line:typedef
-  onSubmit(value: any) {
-    console.log(value);
-  }
 }
-
-export class Form {
-  input: string;
-  validation: boolean;
-
-  constructor(input: string) {
-    this.input = input;
-    this.validation = false;
-  }
-}
-// this.form = {
-//   form2: {
-//     input2: '',
-//     form2_1: {
-//       input2_1: ''
-//     },
-//     form2_2: {
-//       input2_2: ''
-//     }
-//   },
-//   form3: {
-//     input3: '',
-//     form3_1: {
-//       input3_1: '',
-//     }
-//   },
-//   form4: {
-//     input4: '',
-//     form4_1: {
-//       input4_1: ''
-//     }
-//   }
-// };
